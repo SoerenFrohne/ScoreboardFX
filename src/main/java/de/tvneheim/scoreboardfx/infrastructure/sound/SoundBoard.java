0347ae755fa5f3af6216eb4b81dc738b4f740f69
Soeren Frohne <s.frohne@unitybox.de>
2026-06-24 13:43:31 +0200

    feat: preload sound effects to memory to increase performance on audio clips

--- src/main/java/de/tvneheim/scoreboardfx/infrastructure/sound/SoundBoard.java
+++ src/main/java/de/tvneheim/scoreboardfx/infrastructure/sound/SoundBoard.java
@@ -2,28 +2,39 @@ package de.tvneheim.scoreboardfx.infrastructure.sound;
 
 import javafx.scene.media.AudioClip;
 
+import java.util.Objects;
+
 public final class SoundBoard {
 
-  private static final String HORN_LONG = "/de/tvneheim/scoreboardfx/sfx/horn-long.wav";
-  private static final String HORN_SHORT = "/de/tvneheim/scoreboardfx/sfx/horn-short.wav";
+  private static final String HORN_LONG_PATH = "/de/tvneheim/scoreboardfx/sfx/horn-long.wav";
+  private static final String HORN_SHORT_PATH = "/de/tvneheim/scoreboardfx/sfx/horn-short.wav";
+
+  private static final AudioClip HORN_LONG = loadAudioClip(HORN_LONG_PATH);
+  private static final AudioClip HORN_SHORT = loadAudioClip(HORN_SHORT_PATH);
+
 
   private SoundBoard() {
+    // Vorwärmen, damit beim ersten Laden keine Verzögerungen entstehen
+    HORN_SHORT.setVolume(0);
+    HORN_SHORT.play();
+    HORN_SHORT.setVolume(100);
+
+    HORN_LONG.setVolume(0);
+    HORN_LONG.play();
+    HORN_LONG.setVolume(100);
   }
 
   public static void honkShort() {
-    play(HORN_SHORT);
+    HORN_SHORT.play();
   }
 
   public static void honkLong() {
-    play(HORN_LONG);
+    HORN_LONG.play();
   }
 
-  private static void play(String sound) {
-    var sfx = SoundBoard.class.getResource(sound);
-    assert sfx != null;
-    AudioClip honk = new AudioClip(sfx.toExternalForm());
-    honk.play();
+  private static AudioClip loadAudioClip(String path) {
+    return new AudioClip(Objects.requireNonNull(SoundBoard.class.getResource(path)).toExternalForm()
+    );
   }
-
 
 }
